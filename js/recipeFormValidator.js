c6e4ef0c333e93e07600a345f418a53b7bb0e201
Nolan <nicholsn@oregonstate.edu>
2019-04-07 18:18:14 -0700

    Added instruction step functionality
    GIT GOOD

---
+++ js/recipeFormValidator.js
@@ -0,0 +1,199 @@
+//Change the border color depending on the state of the input
+//Accept element and a string color
+function changeBorderColor (element, color){
+	
+	element.style.border = "1px solid " + color;
+}
+
+//Adjust regEx as needed
+function isValidString(string){
+	
+	var pattern = new RegExp("^[a-zA-Z][a-zA-Z0-9 \-']+$");
+	var result= pattern.test(string);
+	
+	//Yay
+	if(result){
+		
+		return true;
+		
+	}
+	//Icky characters found
+	else{
+		
+		return false;
+		
+	}
+
+}
+
+function validateName(){
+	
+	var name = document.getElementById("recipeNameForm");
+	
+	//If improper characters
+	if(isValidString(name.value) === false ){
+		
+		changeBorderColor(name, "red");
+		
+	}
+	//If length is too short or too long. Change me so we can alert user
+	else if(name.value.length < 1 || name.value.length > 100 ){
+		
+		changeBorderColor(name, "red");
+		
+	}
+	else if(name.value == ""){
+		
+		changeBorderColor(name, "red");
+		
+	}
+	else{
+		
+		changeBorderColor(name, "green");
+		
+	}
+	
+}
+
+function validateDescription(){
+	
+	var desc = document.getElementById("recipeDescriptionForm");
+	
+	//For now, just check for length requirements and then implement some regex for sanitation purposes
+	
+	if(desc.value.length < 2){
+		
+		changeBorderColor(desc, "red");
+		
+	}
+	else{
+		
+		changeBorderColor(desc, "green");
+		
+	}
+	
+}
+
+//Expects an integer value
+function validatePrepTime(){
+	
+	var prepTime = document.getElementById("recipePrepTimeForm");
+	
+	if(prepTime.value == ""){
+	
+		changeBorderColor(prepTime, "red");
+	
+	}
+
+	else if(isNaN(prepTime.value)){
+		
+		changeBorderColor(prepTime, "red");
+	}
+	else{
+		changeBorderColor(prepTime, "green");
+	}
+	
+}
+
+//Expects an integer value
+function validateCookTime(){
+	
+	var cookTime = document.getElementById("recipeCookTimeForm");
+	
+	if(cookTime.value == ""){
+	
+		changeBorderColor(cookTime, "red");
+	
+	}
+
+	else if(isNaN(cookTime.value)){
+		
+		changeBorderColor(cookTime, "red");
+	}
+	else{
+		
+		changeBorderColor(cookTime, "green");
+		
+	}
+	
+}
+
+//This function needs so much help...
+function submitFormData(){
+	
+	//Non array vars
+	var name = document.getElementById("recipeNameForm").value;
+	var description = document.getElementById("recipeDescriptionForm").value;
+	var prepTime = document.getElementById("recipePrepTimeForm").value;
+	var cookTime = document.getElementById("recipeCookTimeForm").value;
+	
+	//Array vars for ingredient data
+	var ingredients = document.getElementsByName("ingredientName[]");
+	var qty = document.getElementsByName("qty[]");
+	var unit = document.getElementsByName("unit[]");
+	
+	//Array var for steps
+	var steps = document.getElementsByName("stepDescription[]");
+	
+	//Get the total number of ingredients
+	
+	var length = ingredients.length;
+	
+	//New arrays since ajax doesn't like html -> js
+	//Should just use JSON rather than create all of these arrays...
+	var ingName = [length];
+	var ingQty = [length];
+	var ingUnit = [length];
+	
+	//Copy data from old arrays into JS arrays
+	//Move to own function...
+	for(var i = 0; i < length; i++){
+		
+		ingName[i] = ingredients[i].value;
+		ingQty[i] = qty[i].value;
+		ingUnit[i] = unit[i].value;
+		
+	}
+	
+	
+	//Copy data from old steps array into new onerror
+	var numSteps = steps.length;
+	var newSteps = [numSteps];
+	
+	for(var i = 0; i < numSteps; i++){
+		
+		newSteps[i] = steps[i].value;
+
+		
+	
+	}
+	
+	//Off to the races....
+	$.ajax({
+		
+		type: "POST",
+		url: "php/recipeUploadHelper.php",
+		data: {
+			
+			name: name,
+			description: description,
+			prepTime: prepTime,
+			cookTime: cookTime,
+			ingredients: JSON.stringify(ingName),
+			qty: JSON.stringify(ingQty),
+			unit: JSON.stringify(ingUnit),
+			step: JSON.stringify(newSteps)
+				
+		},
+		
+		success: function(callback){
+			
+			console.log(callback);
+			//Retrieve recipe file directory location for image storage
+		
+		}
+		
+	});
+
+}
+
