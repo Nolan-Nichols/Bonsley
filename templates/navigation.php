83aac2d933ca48f66232396a52cafbdd61976478
Nolan <nicholsn@oregonstate.edu>
2019-04-20 18:21:48 -0700

    Eww

--- templates/navigation.php
+++
@@ -1,9 +0,0 @@
-<div class="topnav">
-			
-			<a href="index.php">Home</a>			
-			<a href="recipe.php">Recipies</a>
-			<a href="create.php">Create</a>
-			<a href="#">Register</a>
-			<a href="#">Login</a>
-			
-		</div>
